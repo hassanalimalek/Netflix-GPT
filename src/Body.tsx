b14e08e0ef918ba8e376dacf339fcb6f99846bac
Hassan Ali <hassanalimalek9@gmail.com>
2024-01-23 07:07:23 +0500

    browser router added

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,10 +1,21 @@
+import { Browse } from './components/browse';
 import { Login } from './components/login';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 export const Body = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/',
+      element: <Browse />,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      {/* <Browse /> */}
+      <RouterProvider router={router} />
     </div>
   );
 };
